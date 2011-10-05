a12616bb15c57e497854f4a74825ed51d43efce7
Eric Platon <zaraki@gmx.com>
2011-10-05 23:31:48 +0900

    Dumber watcher.

--- src/jp/ac/mcg/SimpleWatcher.java
+++ src/jp/ac/mcg/SimpleWatcher.java
@@ -14,18 +14,7 @@ import org.apache.zookeeper.Watcher.Event.KeeperState;
 import org.apache.zookeeper.Watcher.Event.EventType;
 
 class SimpleWatcher implements Watcher {
-    private final CountDownLatch connected;
-
-    public SimpleWatcher(final int clients) {
-      this.connected = new CountDownLatch(clients);
-    }
-
     synchronized public void process(WatchedEvent event) {
-      System.out.println("Event received: " + event);
-      if (event.getState() == KeeperState.SyncConnected ||
-	  event.getState() == KeeperState.ConnectedReadOnly) {
-        connected.countDown();
-      }
       notifyAll();
     }
 }
