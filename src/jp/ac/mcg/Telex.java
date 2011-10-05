75a9b6b3e96f8abd4fbfe852078c638953c3cf5b
Eric Platon <zaraki@gmx.com>
2011-10-05 10:48:36 +0900

    Refactoring in search for a working example.

--- src/jp/ac/mcg/Telex.java
+++ src/jp/ac/mcg/Telex.java
@@ -32,10 +32,10 @@ public class Telex {
   String      vocabulary = "abcdefghijklmnopqrstuvwxyz";
   SimpleWatcher     watcher;
   DistributedQueue[] handles;
-  int       nextPort = 31457;
+  int       nextPort = 3000;
 
   public static void main(final String[] args) {
-    int numConsumers = 2;
+    int numConsumers = 1;
     if(args.length > 0 && args[0] != null) {
       numConsumers = new Integer(args[0]);
     }
@@ -46,27 +46,20 @@ public class Telex {
     try {
       consumers = new ZooKeeper[numConsumers];
       handles   = new DistributedQueue[numConsumers];
-      watcher   = new SimpleWatcher(new CountDownLatch(numConsumers));
+      watcher   = new SimpleWatcher(numConsumers);
 
       for(int i = 0; i < numConsumers; i++) {
 	consumers[i] = new ZooKeeper(host + ":" + assignPort(), TIMEOUT, watcher);
-	if (!watcher.getLatch().await(TIMEOUT, TimeUnit.MILLISECONDS)) {
-          System.out.println("Timeout: Unable to connect to server");
-	  System.exit(1);
-        }
 	handles[i] = new DistributedQueue(consumers[i], directory, null);
       }
     } catch(IOException e) {
       System.out.println("Initialization error: " + e.getMessage());
       System.exit(1);
-    } catch(InterruptedException e) {
-     // e.printStackTrace();
-     // System.exit(3);
     }
   }
 
   public void run() {
-    int numThreads = consumers.length;
+    final int numThreads = consumers.length;
     Thread[] threads = new Thread[numThreads];
 
     for(int i = 0; i < consumers.length; i++) {
@@ -74,6 +67,7 @@ public class Telex {
       threads[i] = new Thread() {
         public void run() {
 	  try {
+	    System.out.println("Consumer " + id + " started.");
             final String msg = new String(handles[id].take());
 	    System.out.println("             " + msg + " > Consumer " + id);
           } catch(KeeperException e) {
@@ -88,18 +82,35 @@ public class Telex {
       threads[i].start();
     }
 
-    try {
-      for(int idx = 0; idx < vocabulary.length() - 1; idx++) {
-	final String msg = vocabulary.substring(idx, idx + 1);
-	System.out.println("Publishing > " + msg);
-	handles[idx % numThreads].offer(msg.getBytes());
+    try{
+      System.out.println("Waiting for setup?");
+      Thread.sleep(1000);
+      System.out.println(handles[0].toString());
+    } catch(InterruptedException e) {}
+
+    Thread publisher = new Thread() {
+      public void run() {
+	try {
+//	  for(int idx = 0; idx < vocabulary.length() - 1; idx++) {
+//	    final String msg = vocabulary.substring(idx, idx + 1);
+	    final String msg = vocabulary.substring(3, 4);
+	    boolean offered = handles[0].offer(msg.getBytes());
+	    System.out.println(offered);
+	    System.out.println("Published > " + msg);
+//	  }
+	} catch(KeeperException e){
+	//  e.printStackTrace();
+	//  System.exit(4);
+	} catch(InterruptedException e){
+	//  e.printStackTrace();
+	//  System.exit(5);
+	}
       }
-    } catch(KeeperException e){
-    //  e.printStackTrace();
-    //  System.exit(4);
+    };
+    publisher.start();
+    try {
+      publisher.join();
     } catch(InterruptedException e){
-    //  e.printStackTrace();
-    //  System.exit(5);
     }
 
     for(int i = 0; i < consumers.length; i++) {
@@ -110,6 +121,13 @@ public class Telex {
 //	System.exit(6);
       }
     }
+
+    //try {
+    //  watcher.getLatch().await(TIMEOUT, TimeUnit.MILLISECONDS);
+    //} catch(InterruptedException e) {
+     // e.printStackTrace();
+     // System.exit(3);
+    //}
 
     System.out.println("Completed.");
   }
